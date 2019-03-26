46c5f57266b7486e7ffe0ae5445b586c2da777c5
Steffen Sande <steffen.sande94@gmail.com>
2019-03-26 18:09:45 +0100

    git commit -m

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,44 @@
 import * as React from "react";
 import NavBar from "./NavBar";
+import {EndPoints} from './Endpoints';
+
 export interface IAppProps {
 }
+
 export interface IAppState {
+    message: string
 }
+
 export default class App extends React.Component<IAppProps, IAppState> {
     constructor(props: IAppProps) {
         super(props);
+        this.state = {
+            message: null
+        }
     }
+
+    componentDidMount() {
+        fetch(EndPoints.TEST)
+            .then((response) => {
+                let message = response.json();
+                console.log(message)
+            })
+            .catch((error) => {
+                console.log("No response or something")
+            })
+    }
+
     render() {
-        return( <div className="App">
-                    <NavBar/>
-                    <h1> Hello from react with typescript scss support! </h1>
-                </div>);
+        if (this.state.message) {
+            return (<div className="App">
+                <NavBar />
+                <h1> {this.state.message} </h1>
+            </div>);
+        } else {
+            return (<div className="App">
+                <NavBar/>
+                <h1> Hello from react with typescript scss support! </h1>
+            </div>);
+        }
     }
 }
