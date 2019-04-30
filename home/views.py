c8335a47d8b5cf3f7c8473692951361df75922e7
Steffen Sande <steffen.sande94@gmail.com>
2019-04-30 16:04:26 +0200

    Endret gitignore og fjernet database dependency

--- home/views.py
+++ home/views.py
@@ -10,15 +10,6 @@ def index(request):
     return render(request, 'frontend/index.html')
 
 
-def test(request):
-    searches = Searches.objects.all()
-
-    return render(request, 'home/test.html', {'searches': searches})
-
-
 @csrf_exempt
 def json(request):
-    if request.method == 'GET':
-        searches = Searches.objects.all()
-        serializers = SearchesSerializer(searches, many=True)
-        return JsonResponse(serializers.data, safe=False)
+    return 'Hei'
--- som/urls.py
+++ som/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import index, test, json
+from home.views import index, json
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('test/', test),
     path('json/', json)
 ]
